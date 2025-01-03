37c094237fcde37a5a022de0f4402c5630272519
jaxmadov <jaxmadov@transoxiania.com>
2025-01-03 20:34:57 +0500

    same changes

--- src/db/migrations/20250103145956_own_transaction.js
+++ src/db/migrations/20250103145956_own_transaction.js
@@ -8,7 +8,7 @@ exports.up = function (knex) {
       table.float('total').notNullable().defaultTo(0); // Umumiy balans
       table.float('credit').notNullable().defaultTo(0); // Kirim(b)
       table.float('provider_fee').notNullable().defaultTo(0); // Click ulushi
-      table.float('tax_it_part').notNullable().defaultTo(0); // IT Park ulushi
+      table.float('tax_it_park').notNullable().defaultTo(0); // IT Park ulushi
       table.float('tax_gov').notNullable().defaultTo(0); // Soliqlar
       table.float('profit').notNullable().defaultTo(0); // Foyda
       table.float('others').notNullable().defaultTo(0); // Balansni tenglash
