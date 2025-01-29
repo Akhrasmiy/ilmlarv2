a74975fc5a108151ecea774408d7ac3cbbab6950
jaxmadov <jaxmadov@transoxiania.com>
2025-01-29 20:31:22 +0500

    subscription

--- src/modules/users/getTeacheraccount.js
+++ src/modules/users/getTeacheraccount.js
@@ -7,9 +7,9 @@ const db = require("../../db/db.js");
 exports.getPublicTeacherAccountService = async (teacherId) => {
   // O'qituvchi haqida asosiy ma'lumot
   const teacher = await db("users")
-    .where({ id: teacherId, type: 1 })
+    .where({ 'users.id': teacherId, 'users.type': 1 })
     .leftJoin("teacher_more_date", "users.id", "teacher_more_date.user_id")
-    .select("users.id","users.type", "users.first_name", "users.last_name", "users.email", "users.profile_img","teacher_more_date.spiceal")
+    .select("users.id", "users.type", "users.first_name", "users.last_name", "users.email", "users.profile_img", "teacher_more_date.spiceal")
     .first();
 
   if (!teacher) {
@@ -21,10 +21,17 @@ exports.getPublicTeacherAccountService = async (teacherId) => {
     .where({ teacher_id: teacherId })
     .select("id");
 
+  // O'qituvchiga obuna bo'lgan studentlar soni
+  const subscribedStudentsCount = await db('subscriptions')
+    .where({ teacher_id: teacherId })
+    .count('student_id as count')
+    .first();
+
   // Ma'lumotlarni birlashtirib qaytarish
   return {
     ...teacher,
     courses,
+    subscribedStudentsCount: subscribedStudentsCount.count
   };
 };
 
