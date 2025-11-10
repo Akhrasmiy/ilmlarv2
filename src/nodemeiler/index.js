8ba004071c7161af44c39da3bb9407da00c04bbd
Jahongir Akhmadov <104265452+Akhrasmiy@users.noreply.github.com>
2025-11-10 17:13:23 +0500

    Update email  in nodemailer configuration

--- src/nodemeiler/index.js
+++ src/nodemeiler/index.js
@@ -10,13 +10,13 @@ const sendEmail = async (email, password) => {
     port: 587,
     secure: false,
     auth: {
-      user: "giybatchat@gmail.com",
-      pass: "ifgmmkgchsuymepg",
+      user: "ilmlar.com.info@gmail.com",
+      pass: "qessxvonxlpjbprb",
     },
   });
   try {
     const info = await transporter.sendMail({
-      from: "giybatchat@gmail.com",
+      from: "ilmlar.com.info@gmail.com",
       to: email,
       subject: "tasdiqlash kodi",
       html: `<h1>${password}<h1>`,
